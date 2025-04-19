1a49c811f8ae0305cbced90f0941c8ba3f2fde6d
Prakash Dantani <110656797+Prakash-Dantani@users.noreply.github.com>
2025-04-19 08:27:01 +0530

    google auth setup

--- index.js
+++ index.js
@@ -3,16 +3,57 @@ const cors = require('cors');
 const routes = require('./routes/index.js');
 const { mongoose } = require('mongoose');
 require('dotenv').config({ path: '.env.development' });
+const passport = require("passport");
+const session = require("express-session");
+
+require("./controllers/passport.js"); // config below
+
 
 const app = express();
 
 app.use(cors({
     origin: 'http://localhost:5173',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization']
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true, // Allow cookies to be sent
+
 }));
-app.use(express.json())
+app.use(express.json());
+
+
+//code for Google Auth Login
+app.use(session({
+    secret: "secret", resave: false, saveUninitialized: true,
+    cookie: {
+        // expires: new Date(Date.now() + 1000 * 60 * 60 * 24), // Expires in 1 day from now
+        // maxAge: 1000 * 60 * 60 * 24 // Set the session to expire in 24 hours
+        maxAge: 1000 * 60 * 60 * 24, httpOnly: true, secure: false  // Set 'secure: true' for HTTPS
+
+    }
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+
 app.use("/api", routes);
+
+app.get('/auth/google',
+    passport.authenticate('google', { scope: ['profile', 'email'] })
+);
+
+app.get('/auth/google/callback',
+    passport.authenticate('google', { failureRedirect: '/login' }),
+    (req, res) => {
+        // Successful login
+        res.redirect('http://localhost:5173/'); // frontend URL
+    }
+);
+
+app.get('/logout', (req, res) => {
+    req.logout(() => {
+        res.redirect('/');
+    });
+});
 
 
 mongoose.connect(process.env.MONGO_URI, {
