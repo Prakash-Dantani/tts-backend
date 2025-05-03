dfd6fdabc8c15e05e47571bc487816598632fa47
Prakash Dantani <110656797+Prakash-Dantani@users.noreply.github.com>
2025-05-03 09:01:06 +0530

    change in voiceController.js

--- controllers/voiceController.js
+++ controllers/voiceController.js
@@ -2,9 +2,21 @@ const { Voices } = require("../models/Voices");
 
 const getVoices = async (req, res) => {
     try {
-        const voices = await Voices.find();
-        res.status(200).json({ "message": "Successfully Get All Voices.", "voices": voices });
+        const page = parseInt(req.query.page) || 1;
+        const limit = parseInt(req.query.limit) || 10;
+        const skip = (page - 1) * limit;
+
+        const total = await Voices.countDocuments(); // ✅ Required
+
+        const voices = await Voices.find().skip(skip)
+            .limit(limit);
+
+        res.status(200).json({
+            "message": "Successfully Get All Voices.", "voices": voices, "currentPage": page,
+            "totalPages": Math.ceil(total / limit)
+        });
     } catch (error) {
+        console.error(error.message);
         res.status(500).json({ "message": "server Error encounterd." })
     }
 };
