e4964bf9a9f0c6519b2fc1101fd5423e57c5c5f9
Prakash Dantani <110656797+Prakash-Dantani@users.noreply.github.com>
2025-08-08 09:06:19 +0530

    Pull and push of main

--- controllers/passport.js
+++ controllers/passport.js
@@ -14,7 +14,7 @@ passport.deserializeUser(async (id, done) => {
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "https://tts-backend-ql5t.onrender.com/auth/google/callback"
+    callbackURL: process.env.GOOGLE_CALLBACK_URL,
 },
     async (accessToken, refreshToken, profile, done) => {
         let user = await User.findOne({ googleId: profile.id });
